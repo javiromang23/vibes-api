356a7117c43a92dfedb9aff570c93c6636ccd8a8
Javier <javiromang23>
2019-05-17 17:12:17 +0200

    Método para guardar una notificación y obtener todas las de un usuario.

--- src/controllers/notification.js
+++ src/controllers/notification.js
@@ -7,6 +7,50 @@ const parameters = require("../parameters");
 const notificationController = {
   test: (req, res) => {
     res.status(200).send({ message: "Testing notificationController..." });
+  },
+  saveNotification: (req, res) => {
+    if (
+      req.params.user != "" &&
+      req.params.user != undefined &&
+      req.body.html != undefined &&
+      req.body.html != ""
+    ) {
+      let notification = new Notification();
+      notification.html = req.body.html;
+      notification.user = req.params.user;
+
+      Notification.create(notification, (err, notificationStored) => {
+        if (err)
+          return res.status(500).send({ message: `Error server: ${err}` });
+        if (!notificationStored)
+          return res
+            .status(401)
+            .send({ message: `The notification could not be saved.` });
+
+        return res.status(200).send({
+          message: "Notication stored.",
+          publication: notificationStored
+        });
+      });
+    } else {
+      return res
+        .status(400)
+        .send({ message: "Missing arguments or are invalid" });
+    }
+  },
+  getNotificationsByUser: (req, res) => {
+    Notification.find({ user: req.user }, (err, notifications) => {
+      if (err) return res.status(500).send({ message: `Error server: ${err}` });
+      if (!notifications)
+        return res
+          .status(401)
+          .send({ message: `The notification could not be saved.` });
+
+      return res.status(200).send({
+        message: "Notications found.",
+        notifications: notifications
+      });
+    });
   }
 };
 
--- src/routes/notification.js
+++ src/routes/notification.js
@@ -6,5 +6,7 @@ const auth = require("../middlewares/auth");
 const api = express.Router();
 
 api.get("/test-notification", notificationController.test);
+api.post("/notification/:user", auth, notificationController.saveNotification);
+api.get("/notification", auth, notificationController.getNotificationsByUser);
 
 module.exports = api;
