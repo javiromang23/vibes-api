c986a5c01941c3b3f2763876f071f28c7f746b26
Javier <javiromang23>
2019-05-17 15:44:52 +0200

    Añadido métodos para actualizar, eliminar y devolver comentarios de una publicación.

--- src/controllers/comment.js
+++ src/controllers/comment.js
@@ -57,6 +57,142 @@ const commentController = {
         .status(400)
         .send({ message: "Missing arguments or are invalid" });
     }
+  },
+  updateComment: async (req, res) => {
+    if (
+      req.params.comment != "" &&
+      req.params.comment != undefined &&
+      req.body.text != "" &&
+      req.body.text != undefined
+    ) {
+      try {
+        let comment = await Comment.findById(req.params.comment);
+
+        let publication = await Publication.findById(
+          comment.publication
+        ).populate("user");
+        if (!publication)
+          return res.status(404).send({ message: "Publication not found" });
+
+        if (publication.user.typeAccount != parameters.typeAccount.public) {
+          let follow = await Follow.findOne({
+            user: req.user,
+            followed: publication.user.id,
+            toAccept: true
+          });
+          if (!follow)
+            return res.status(403).send({
+              message:
+                "Forbidden: You don't have permission to access on this user"
+            });
+        }
+        comment.text = req.body.text;
+
+        let commentUpdated = await Comment.findOneAndUpdate(
+          { _id: comment.id },
+          comment,
+          { new: true }
+        );
+        if (!commentUpdated)
+          return res.status(404).send({
+            message: "Error in the request."
+          });
+
+        return res
+          .status(200)
+          .send({ message: "Comment updated", comment: commentUpdated });
+      } catch (err) {
+        return res.status(500).send({ message: `Error server: ${err}` });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ message: "Missing arguments or are invalid" });
+    }
+  },
+  deleteComment: async (req, res) => {
+    if (req.params.comment != "" && req.params.comment != undefined) {
+      try {
+        let comment = await Comment.findById(req.params.comment);
+
+        let publication = await Publication.findById(
+          comment.publication
+        ).populate("user");
+        if (!publication)
+          return res.status(404).send({ message: "Publication not found" });
+
+        if (publication.user.typeAccount != parameters.typeAccount.public) {
+          let follow = await Follow.findOne({
+            user: req.user,
+            followed: publication.user.id,
+            toAccept: true
+          });
+          if (!follow)
+            return res.status(403).send({
+              message:
+                "Forbidden: You don't have permission to access on this user"
+            });
+        }
+
+        let commentDeleted = await Comment.findOneAndDelete({
+          _id: comment.id
+        });
+        if (!commentDeleted)
+          return res.status(404).send({
+            message: "Error in the request."
+          });
+
+        return res.status(200).send({ message: "Comment deleted" });
+      } catch (err) {
+        return res.status(500).send({ message: `Error server: ${err}` });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ message: "Missing arguments or are invalid" });
+    }
+  },
+  getCommentsByPublication: async (req, res) => {
+    if (req.params.publication != "" && req.params.publication != undefined) {
+      try {
+        let publication = await Publication.findById(
+          req.params.publication
+        ).populate("user");
+        if (!publication)
+          return res.status(404).send({ message: "Publication not found" });
+
+        if (publication.user.typeAccount != parameters.typeAccount.public) {
+          let follow = await Follow.findOne({
+            user: req.user,
+            followed: publication.user.id,
+            toAccept: true
+          });
+          if (!follow)
+            return res.status(403).send({
+              message:
+                "Forbidden: You don't have permission to access on this user"
+            });
+        }
+
+        let commentsFound = await Comment.find({
+          publication: req.params.publication
+        });
+        if (!commentsFound)
+          return res.status(404).send({
+            message: "Error in the request."
+          });
+
+        return res
+          .status(200)
+          .send({ message: "Comments found", comments: commentsFound });
+      } catch (err) {
+        return res.status(500).send({ message: `Error server: ${err}` });
+      }
+    } else {
+      return res
+        .status(400)
+        .send({ message: "Missing arguments or are invalid" });
+    }
   }
 };
 
--- src/routes/comment.js
+++ src/routes/comment.js
@@ -7,5 +7,12 @@ const api = express.Router();
 
 api.get("/test-comment", commentController.test);
 api.post("/comment/:publication", auth, commentController.saveComment);
+api.put("/comment/:comment", auth, commentController.updateComment);
+api.delete("/comment/:comment", auth, commentController.deleteComment);
+api.get(
+  "/comment/:publication",
+  auth,
+  commentController.getCommentsByPublication
+);
 
 module.exports = api;
